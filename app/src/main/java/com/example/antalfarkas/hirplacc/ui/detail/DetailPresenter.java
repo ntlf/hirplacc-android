ea918673f63741b7b5733c077ccb0cfe3e4dde55
Antal Farkas <farkasantal.hun@gmail.com>
2018-04-09 07:57:34 +0200

    Added interactors

--- app/src/main/java/com/example/antalfarkas/hirplacc/ui/detail/DetailPresenter.java
+++ app/src/main/java/com/example/antalfarkas/hirplacc/ui/detail/DetailPresenter.java
@@ -1,8 +1,15 @@
 package com.example.antalfarkas.hirplacc.ui.detail;
 
+import com.example.antalfarkas.hirplacc.interactor.news.NewsInteractor;
 import com.example.antalfarkas.hirplacc.ui.Presenter;
 
+import javax.inject.Inject;
+
 public class DetailPresenter extends Presenter<DetailScreen> {
+    @Inject
+    NewsInteractor newsInteractor;
+
+
     @Override
     public void attachScreen(DetailScreen screen){
         super.attachScreen(screen);
--- app/src/main/java/com/example/antalfarkas/hirplacc/ui/main/MainPresenter.java
+++ app/src/main/java/com/example/antalfarkas/hirplacc/ui/main/MainPresenter.java
@@ -1,8 +1,14 @@
 package com.example.antalfarkas.hirplacc.ui.main;
 
+import com.example.antalfarkas.hirplacc.interactor.news.NewsInteractor;
 import com.example.antalfarkas.hirplacc.ui.Presenter;
 
+import javax.inject.Inject;
+
 public class MainPresenter extends Presenter<MainScreen> {
+    @Inject
+    NewsInteractor newsInteractor;
+
     @Override
     public void attachScreen(MainScreen screen){
         super.attachScreen(screen);
